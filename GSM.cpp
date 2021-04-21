641ee5ab464cab2e4686a8d1df480b30521daad3
oussamabouguerra2019 <55598245+oussamabouguerra2019@users.noreply.github.com>
2021-04-21 05:43:33 -0700

    Update GSM.cpp

--- GSM.cpp
+++ GSM.cpp
@@ -24,8 +24,8 @@ based on QuectelM10 chip.
 
 //De-comment this two lines below if you have the
 //second version og GSM GPRS Shield
-#define _GSM_TXPIN_ 2
-#define _GSM_RXPIN_ 3
+#define _GSM_TXPIN_ 10
+#define _GSM_RXPIN_ 9
 
 #ifdef UNO
 GSM::GSM():_cell(_GSM_TXPIN_,_GSM_RXPIN_),_tf(_cell, 10),_status(IDLE)
